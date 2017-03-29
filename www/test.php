86d02352b841f6fd1cd5508d8e9d23052972b5a4
dimeji <dimeji.taiwo5@outlook.com>
2017-03-29 11:51:11 +0100

    way to go

--- www/test.php
+++ www/test.php
@@ -22,11 +22,68 @@ try {
 	echo $e->getMessage();
 }
 
+define('MAX_FILE_SIZE','2097152');
+
 if (array_key_exists('save', $_POST)){
 
-	print_r ($_FILES);
+	$errors = [];
+
+	#to be sure a file was selected...
+
+	if (empty($_FILES['pic']['name'])){
+
+		$errors[] = "please choose a file";
+	}
+
+	#check file size
+
+	if ($_FILES['pic']['size'] > MAX_FILE_SIZE){
+
+		$errors[] = "file size exceeds maximum. maximum:". MAX_FILE_SIZE;
+	}
+
+	#check extensions
+
+	if (!in_array($_FILES['pic']['type'],$ext)){
+
+		$errors[] = "invalid file type";
+	}
+
+
+	#generate random number to append....
+
+	$rnd = rand (0000000000,9999999999);
+
+
+	#strip filename for spaces
+
+	$strip_name = $str_replace ("","",$_FILES['pic']['name']);
+
+	$filename = $rnd.$strip_name;
+	$destination = 'uploads/' .$filename;
+
+	if (!move_uploaded_file($_FILES['pic']['tmp_name'], $destination)){
+
+		$errors[] = "file upload failed" ;
+	}
+
+		if(empty($errors)){
+
+			echo "done";
+
+
+		} else {
+
+			foreach($errors as $error){
+
+				echo $error;
+		}
+	}
 
 }
+
+
+
 
 ?>
 
