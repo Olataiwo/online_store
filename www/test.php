43fd02592d1928bfddad1eb2cadeea103ddea430
dimeji <dimeji.taiwo5@outlook.com>
2017-03-29 09:18:08 +0100

    Exception error completed

--- www/test.php
+++ www/test.php
@@ -5,7 +5,21 @@ define('DBNAME','test');
 define('DBUSER','root');
 define('DBPASS','riddle1');
 
-$conn = new PDO("mysql:host=localhost;dbname=".DBNAME,DBUSER,DBPASS);
 
+
+try {
+
+	# prepare a pdo instance
+	$conn = new PDO("mysql:host=localhost;dbname=".DBNAME,DBUSER,DBPASS);
+
+	#set Verbose error modes
+
+	$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_SILENT);
+
+
+} catch (PDOException $e){
+
+	echo $e->getMessage();
+}
 
 ?>
