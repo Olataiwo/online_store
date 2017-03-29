495b44fff3868515c4bdb67b36166d309abdfae9
dimeji <dimeji.taiwo5@outlook.com>
2017-03-29 12:21:55 +0100

    includes/footer.php

---
+++ www/login.php
@@ -0,0 +1,46 @@
+<?php
+
+
+
+
+
+include 'includes/header.php';
+
+
+?>
+
+	
+	<div class="wrapper">
+		<h1 id="register-label">Admin Login</h1>
+		<hr>
+		<form id="register"  action ="login.php" method ="POST">
+			<div>
+				<label>email:</label>
+				<input type="text" name="email" placeholder="email">
+			</div>
+			<div>
+				<label>password:</label>
+				<input type="password" name="password" placeholder="password">
+			</div>
+
+			<input type="submit" name="register" value="login">
+		</form>
+
+		<h4 class="jumpto">Don't have an account? <a href="register.php">register</a></h4>
+	</div>
+
+	<section class="foot">
+		<div>
+			<p>&copy; 2016;
+		</div>
+	</section>
+</body>
+</html>
+
+
+	<?php
+
+	include 'includes/footer.php';
+
+
+	?>
