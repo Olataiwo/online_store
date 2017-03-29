da8387ca47bbdf06a39fbec8c474c7378331ecc9
dimeji <dimeji.taiwo5@outlook.com>
2017-03-29 10:30:23 +0100

    good to go

--- www/test.php
+++ www/test.php
@@ -42,20 +42,48 @@ if (array_key_exists('save', $_POST)){
 		$errors[] = "file size exceeds maximum. maximum:". MAX_FILE_SIZE;
 	}
 
-	if(empty($errors)){
+	#check extensions
 
-		echo "done";
+	if (!in_array($_FILES['pic']['type'],$ext)){
+
+		$errors[] = "invalid file type";
+	}
 
 
-	} else {
+	#generate random number to append....
 
-		foreach($errors as $error){
+	$rnd = rand (0000000000,9999999999);
 
-			echo $error;
+
+	#strip filename for spaces
+
+	$strip_name = $str_replace ("","",$_FILES['pic']['name']);
+
+	$filename = $rnd.$strip_name;
+	$destination = 'uploads/' .$filename;
+
+	if (!move_uploaded_file($_FILES['pic']['tmp_name'], $destination)){
+
+		$errors[] = "file upload failed" ;
+	}
+
+		if(empty($errors)){
+
+			echo "done";
+
+
+		} else {
+
+			foreach($errors as $error){
+
+				echo $error;
 		}
 	}
 
 }
+
+
+
 
 ?>
 
